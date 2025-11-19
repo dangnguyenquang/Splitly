b3ab20228f45b2781941366ab779aa49bb8c3204
Loc Nguyen <126844791+iseT1enLoc@users.noreply.github.com>
2025-11-19 15:20:15 +0700

    Change some setup to make it runnable on Loc's local computer
    ^ Conflicts:
    ^	src/main/resources/application.yaml

--- src/main/java/com/example/splitly/SplitlyApplication.java
+++ src/main/java/com/example/splitly/SplitlyApplication.java
@@ -2,10 +2,12 @@ package com.example.splitly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+// @EnableJpaRepositories("com.example.splitly.domain.repository")
 public class SplitlyApplication {
 
 	public static void main(String[] args) {
